06739a41e08e240f93eb1c2f868cdaef145e235a
aimishan <aimishan@163.com>
2017-03-09 11:15:24 +0800

    现在可以查看http头信息了

--- srcexplorer.py
+++ srcexplorer.py
@@ -4,11 +4,14 @@ url =''
 string = ''
 r = None
 sourcecode = ''
+webpage_info = ''
 
 def sendMessage(e=None):
 	global string
 	global url
 	global sourcecode
+	global webpage_info
+	global r
 	infobox.delete(0, tk.END)
 	string = entry.get()
 	if string[0] == ':':
@@ -21,7 +24,9 @@ def sendMessage(e=None):
 	if string[0:4] == 'http':
 		url = string
 		sourcecode = getSourceCode(url)
+		webpage_info = getHeaders(r)
 		display(sourcecode)
+		display(webpage_info, 1)
 	else:
 		display(invalid_info)
 
@@ -30,10 +35,20 @@ def getSourceCode(url):
 	r = requests.get(url)
 	return bs4.BeautifulSoup(r.content.decode('utf-8'), 'lxml').prettify()
 
-def display(rawstr):
+def getHeaders(req):
+	tmp = ''
+	for key in req.headers.keys():
+		tmp = tmp + key + '\n' + '     ' + req.headers[key] + '\n'
+	return tmp
+
+def display(rawstr, index=0):
 	consequence = rawstr.split('\n')
-	for line in consequence:
-		infobox.insert(tk.END, line)
+	if index == 0:
+		for line in consequence:
+			infobox.insert(tk.END, line)
+	else:
+		for line in consequence:
+			headerinfo.insert(tk.END, line)
 
 help_info = 'HELP:\nAuthor: Aimishan\nDate: 2017/03/07\n'
 version_info = 'Version: 0.0.1'
@@ -43,9 +58,11 @@ win = tk.Tk()
 win.title('网站源代码查看器')
 
 entry = tk.Entry(win, width=100)
-infobox = tk.Listbox(win, width=90, height=30, background='grey')
+infobox = tk.Listbox(win, width=90, height=35, background='grey')
 scrollbar = tk.Scrollbar(win)
 downsidescr = tk.Scrollbar(win, orient=tk.HORIZONTAL)
+headerinfo = tk.Listbox(win, width=40)
+headerscr = tk.Scrollbar(win, orient=tk.HORIZONTAL)
 
 scrollbar['command'] = infobox.yview
 infobox['yscrollcommand'] = scrollbar.set
@@ -53,10 +70,15 @@ infobox['yscrollcommand'] = scrollbar.set
 downsidescr['command'] = infobox.xview
 infobox['xscrollcommand'] = downsidescr.set
 
+headerscr['command'] = headerinfo.xview
+headerinfo['xscrollcommand'] = headerscr.set
+
 entry.grid(row=0, column=0, sticky=tk.N+tk.W+tk.E)
 infobox.grid(row=1, column=0, sticky=tk.N+tk.W+tk.E)
 scrollbar.grid(row=1, column=0, sticky=tk.N+tk.S+tk.E)
 downsidescr.grid(row=1, column=0, sticky=tk.S+tk.W+tk.E)
+headerinfo.grid(row=0, column=1, rowspan=2, sticky=tk.N+tk.S+tk.W)
+headerscr.grid(row=1, column=1, sticky=tk.W+tk.E+tk.S)
 
 entry.bind('<Key-Return>', sendMessage)
 
